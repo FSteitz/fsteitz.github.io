d0f5b7fef8957036eae14febf59762f8a5721666
Florian Steitz <florian@fsteitz.com>
2025-07-28 22:38:46 +0200

    Add section for page links to page footer
    First page link:
    - Imprint

--- src/components/page/footer/PageFooter.tsx
+++ src/components/page/footer/PageFooter.tsx
@@ -1,5 +1,7 @@
 import { config } from "@/config"
 
+import { IMPRINT_BASE_PATH } from "@/lib/constants"
+
 import Link from "next/link"
 import { FaGithub, FaLinkedin, FaXing } from "react-icons/fa"
 import { FaBluesky } from "react-icons/fa6"
@@ -7,6 +9,18 @@ import { FaBluesky } from "react-icons/fa6"
 export const PageFooter = () => {
     return (
         <footer className="container mx-auto mt-10 ">
+            <hr className="mx-16 mt-20 mb-12" />
+            <section className="w-[80%] flex flex-row items-center justify-between mx-auto">
+                <div className="mx-auto">
+                    <h4 className="font-sans font-semibold text-xl pb-3">More Pages</h4>
+                    <ul className="text-center">
+                        <li className="font-semibold">
+                            <Link href={`/${IMPRINT_BASE_PATH}`} className="hover:text-sky-600">Imprint</Link>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr className="mx-16 my-12" />
             <section className="w-[80%] flex flex-row items-center justify-between pb-6 mx-auto">
                 <p className="font-semibold">© Copyright {new Date().getFullYear()} {config.website.copyright}</p>
                 <div className="flex flex-row items-center justify-between gap-3">

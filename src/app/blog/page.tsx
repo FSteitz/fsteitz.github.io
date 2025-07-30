8aaca6e4f15c95f64e5c6b65f52861630d726600
Florian Steitz <florian@fsteitz.com>
2025-07-30 21:10:21 +0200

    Add RSS feed info box to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,7 @@
 import { config } from "@/config";
 
+import { Rss } from "lucide-react";
+
 import { PagedBlogPostPreviewGrid } from "@/components/blog/PagedBlogPostPreviewGrid";
 import { MarkdownProse } from "@/components/markdown/MarkdownProse";
 import { PageFrame } from "@/components/page/frame/PageFrame";
@@ -10,7 +12,7 @@ import { markdownContent } from "@/text/markdown";
 
 export default function Blog() {
   return (
-    <PageFrame pageTitle="Empowering Your Tech Journey: Discover Tutorials and Insights on Technologies" pageSubTitle={`by ${config.website.name}`}>
+    <PageFrame pageTitle="Empowering Your Tech Journey: Discover Tutorials and Insights on Technologies" infoBox={{ Icon: Rss, infoText: markdownContent.rss.blog }} pageSubTitle={`by ${config.website.name}`}>
       <MarkdownProse markdownContent={markdownContent.page.introduction.blogPage} />
       <PagedBlogPostPreviewGrid postLimit={BLOG_POSTS_PER_PAGE} page={1} />
     </PageFrame>
